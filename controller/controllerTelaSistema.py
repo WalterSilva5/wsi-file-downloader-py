51889d91c56a4ecec1475def47030d4604cf5537
waltersilva5 <waltersilva5@outlook.com>
2020-01-03 20:21:36 -0300

    tela de download finalizada

--- controller/controllerTelaSistema.py
+++ controller/controllerTelaSistema.py
@@ -1,8 +1,9 @@
+from __future__ import unicode_literals
 from PyQt5.QtWidgets import QMainWindow
-from view.telaSistema import Ui_janelaPrincipal as Ui_MainWindow
+from view.telaSistema import  Ui_MainWindow
 import random, threading, time
-from datetime import datetime
-
+from pytube import YouTube
+import threading
 
 class ControllerTelaSistema(QMainWindow):
     def __init__(self, model):
@@ -10,19 +11,26 @@ class ControllerTelaSistema(QMainWindow):
         self.model = model
         self.tela = Ui_MainWindow()
         self.tela.setupUi(self)
-        self.tela.botaoEntrar.clicked.connect(self.exibirConteudo)
-        contar = threading.Thread(target = self.contarSegundos)
-        contar.daemon = True
-        contar.start()
+    
+        self.tela.buttonBaixar.clicked.connect(self.baixar)
 
-    def exibirConteudo(self):
-        login = self.tela.entradaNome.toPlainText()
-        senha = self.tela.entradaSenha.toPlainText()
-        self.tela.labelErro.setText("logado!")
+    def baixar(self):
+        self.downloader(self.tela.entradaLinkDeDownload.text())        
+    
 
-    def contarSegundos(self):
-        while True:
-            now = datetime.now()
-            self.tela.relogio.setText(now.strftime("%H:%M:%S"))
-            time.sleep(1)
 
+    def downloader(self, link):
+        self.completed = 0
+
+        # creating YouTube object
+        musica = YouTube(link, on_progress_callback=self.acrescentarProgresso)
+        stream = musica.streams.filter(only_audio=True).first()
+        stream.download()
+
+        progredir = threading.Thread(target = self.acrescentarProgresso)
+        progredir.daemon = True
+        progredir.start()  
+    
+    def acrescentarProgresso(self,stream, chunk,file_handle, bytes_remaining):
+        self.completed+=1
+        self.tela.progresso.setValue(self.completed)
